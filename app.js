62d277e0adb6359c72802cae5185490ed2550d51
Christian <christianmxfantin@gmail.com>
2021-11-14 21:45:57 -0300

    v1.0.0

--- app.js
+++ app.js
@@ -29,6 +29,12 @@ const options = {
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 };
 
 const connection = mysql.createConnection(options);

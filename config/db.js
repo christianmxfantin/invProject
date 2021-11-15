14a415c5eae41dab2c5acb81a7d77604872edd7a
Christian <christianmxfantin@gmail.com>
2021-11-14 21:12:52 -0300

    v1.0.0

--- config/db.js
+++ config/db.js
@@ -16,7 +16,6 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    use_env_variable: process.env.DB_URL,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
